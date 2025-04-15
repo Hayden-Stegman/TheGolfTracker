d6a90d3d17b0b318a983fb036e451266d90574a0
Hayden-Stegman <hayden.stegman@gmail.com>
2025-04-14 23:03:37 -0400

    Styled Theme & App Clearing

---
+++ src/themes/theme.ts
@@ -0,0 +1,29 @@
+interface Theme {
+    colorPalette: {
+        white: string,
+        grey500: string,
+        grey400: string,
+        grey300: string,
+        grey200: string,
+        grey100: string,
+        altGreen300: string,
+        altGreen200: string,
+        greenPrimary: string,
+        greenSecondary: string
+    };
+}
+
+export const theme: Theme = {
+    colorPalette: {
+        white: '#EBEBEA',
+        grey500: '#95928C',
+        grey400: '#A4A19A',
+        grey300: '#B4B1A9',
+        grey200: '#C6C3BA',
+        grey100: '#DAD7CD',
+        altGreen300: '#A3B18A',
+        altGreen200: '#BFC4AC',
+        greenPrimary: '#3A5A40',
+        greenSecondary: '#588157'
+    },
+};
